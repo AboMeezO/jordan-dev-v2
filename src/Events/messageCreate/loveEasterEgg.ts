18ddf272d96b98749f89914d7eba67dd6de1a7c6
AboMeezO <abomeezo2@gmail.com>
2026-06-14 18:31:48 +0300

    feat(bot): add owner love easter egg

---
+++ src/Events/messageCreate/loveEasterEgg.ts
@@ -0,0 +1,27 @@
+import type { Message } from "discord.js";
+
+const OWNER_USER_ID = "1505948082041393274";
+const LOVE_YOU_PATTERN = /\blove\s+(?:you|u)\b/i;
+const LOVE_REPLIES = [
+  "Love you too babe",
+  "Love you more",
+  "Always, babe",
+  "Right back at you",
+  "You know I do",
+] as const;
+
+export default async function (message: Message): Promise<void> {
+  if (message.author.bot || message.author.id !== OWNER_USER_ID) {
+    return;
+  }
+
+  if (!LOVE_YOU_PATTERN.test(message.content)) {
+    return;
+  }
+
+  await message.reply(randomLoveReply());
+}
+
+function randomLoveReply(): string {
+  return LOVE_REPLIES[Math.floor(Math.random() * LOVE_REPLIES.length)] ?? LOVE_REPLIES[0];
+}
