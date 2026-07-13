d7be3736cb4b8efd0746b3853cf63b2638ba1c99
Ahmad Naji Mahmoud Kamamji <ahmadnajikam@gmail.com>
2026-07-13 20:04:28 +0300

    fix: add neccessary changes to fix compilation errors

--- packages/ai/src/built-in-tools.ts
+++ packages/ai/src/built-in-tools.ts
@@ -1,9 +1,9 @@
 import { createEmbed } from "./tools/create-embed.js";
-import { getChannelById } from "./tools/get-channel.js";
 import { getCurrentBotInfo } from "./tools/get-bot-info.js";
+import { getChannelById } from "./tools/get-channel.js";
 import { getGuildById } from "./tools/get-guild.js";
-import { getUserById } from "./tools/get-user.js";
 import { getMemberById } from "./tools/get-member.js";
+import { getUserById } from "./tools/get-user.js";
 
 export const defaultTools = {
 	createEmbed,
